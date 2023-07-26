88aca41d09b89d6b86bf2ffb4aa2dbc5c2d39e48
Hossein Mansoori <homassns@yahoo.com>
2023-07-26 05:18:08 -0700

    نمایش داشبورد در اضافه کردن کلاس

--- html/add-class.php
+++ html/add-class.php
@@ -42,7 +42,7 @@ if (isset($_POST['submit-btn'])) {
     <div class="layout-container">
 
 
-      <aside id="layout-menu" style="display: none" class="layout-menu menu-vertical menu bg-menu-theme">
+      <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
         <div class="app-brand demo">
           <a href="index.html" class="app-brand-link">
             <span class="app-brand-logo demo">
