9d236be26fba5c38ee5889daa99e194b386891b9
Hossein-pc <mansoorihossein.g@gmail.com>
2023-09-09 16:36:59 +0330

    بررسی مشکلات

--- assets/connect.php
+++ assets/connect.php
@@ -3,9 +3,9 @@
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
     PDO::ERRMODE_WARNING
   );
-  $pdo = new PDO('mysql:host=localhost;dbname=1402s1403', 'hossein', '1234', $utf);
-  $conn = mysqli_connect("localhost", "hossein", "1234", "");
-  mysqli_select_db($conn, '1402s1403');
+  $pdo = new PDO('mysql:host=localhost;dbname=vswtsdio_1402s1403', 'vswtsdio_hossein', '8v6lZR0S@d3x*Z', $utf);
+  $conn = mysqli_connect("localhost", 'vswtsdio_hossein', '8v6lZR0S@d3x*Z', "");
+  mysqli_select_db($conn, 'vswtsdio_1402s1403');
   session_start();
   date_default_timezone_set("Asia/Tehran");
   include_once 'functions.php';
