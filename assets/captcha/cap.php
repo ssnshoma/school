d0196bd5d078667a7ac0e783e19ae0a92f88b171
Hossein-pc <mansoorihossein.g@gmail.com>
2023-10-21 19:45:04 +0330

    انضاطی

---
+++ assets/captcha/cap.php
@@ -0,0 +1,28 @@
+<?php
+  session_start();
+  $charcters = "";
+  $Chars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  for ($i = 0; $i <= 5; $i++) {
+    $charcters = $charcters . $Chars[rand(0, sizeof($Chars) - 1)];
+  }
+  $captcha_code = $charcters;
+  $target_layer = imagecreatetruecolor(180, 60);
+  $captcha_bacground = imagecolorallocate($target_layer, 255, 255, 255);
+  imagefill($target_layer, 0, 0, $captcha_bacground);
+  $captcha_text_color = imagecolorallocate($target_layer, 39, 55, 70);
+  $font_size = 35;
+  $image_width = 80;
+  $image_height = 48;
+  $line_color = imagecolorallocate($target_layer, 64, 64, 64);
+  for ($i = 0; $i < 6; $i++) {
+    imageline($target_layer, 0, rand() % 50, 20, rand() % 50, $line_color);
+  }
+  $pixel_color = imagecolorallocate($target_layer, 0, 0, 255);
+  for ($i = 0; $i < 1000; $i++) {
+    imagesetpixel($target_layer, rand() % 200, rand() % 100, $pixel_color);
+  }
+  imagettftext($target_layer, $font_size, 0, 15, 45, $captcha_text_color, "Wicked Autumn.ttf", $captcha_code);
+  header("Content-Type: image/jpeg");
+  imagejpeg($target_layer);
+  $_SESSION['captcha-code'] = $captcha_code;
+?>
--- assets/login-head.php
+++ assets/login-head.php
@@ -1,106 +1,126 @@
 <!DOCTYPE html>
 <html
- lang="en"
- class="light-style customizer-hide"
- dir="rtl"
- data-theme="theme-default"
- data-assets-path="../assets/"
- data-template="vertical-menu-template-free"
+  lang="en"
+  class="light-style customizer-hide"
+  dir="rtl"
+  data-theme="theme-default"
+  data-assets-path="../assets/"
+  data-template="vertical-menu-template-free"
 >
 <head>
- <meta charset="utf-8"/>
- <meta
-  name="viewport"
-  content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
- />
- <title> ورود </title>
- <meta name="description" content=""/>
- <link rel="icon" type="image/x-icon" href="../assets/img/favicon/favicon.ico"/>
- <link rel="preconnect" href="https://fonts.googleapis.com"/>
- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
- <link
-  href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-  rel="stylesheet"
- />
- <link rel="stylesheet" href="../assets/vendor/fonts/boxicons.css"/>
- <link rel="stylesheet" href="../assets/vendor/css/core.css" class="template-customizer-core-css"/>
- <link rel="stylesheet" href="../assets/vendor/css/theme-default.css" class="template-customizer-theme-css"/>
- <link rel="stylesheet" href="../assets/css/demo.css"/>
- <link rel="stylesheet" href="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css"/>
- <link rel="stylesheet" href="../assets/vendor/css/pages/page-auth.css"/>
- <script src="../assets/vendor/js/helpers.js"></script>
- <script src="../assets/js/config.js"></script>
- <style>
-     #toastBox {
-         position: absolute;
-         top: 30px;
-         right: 45.8%;
-         display: flex;
-         align-items: flex-end;
-         flex-direction: column;
-         overflow: hidden;
-         padding: 20px;
-         z-index: 3000;
-     }
+  <meta charset="utf-8"/>
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+  />
+  <title> ورود </title>
+  <meta name="description" content=""/>
+  <link rel="icon" type="image/x-icon" href="../assets/img/favicon/favicon.ico"/>
+  <link rel="preconnect" href="https://fonts.googleapis.com"/>
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+    rel="stylesheet"
+  />
+  <link rel="stylesheet" href="../assets/vendor/fonts/boxicons.css"/>
+  <link rel="stylesheet" href="../assets/vendor/css/core.css" class="template-customizer-core-css"/>
+  <link rel="stylesheet" href="../assets/vendor/css/theme-default.css" class="template-customizer-theme-css"/>
+  <link rel="stylesheet" href="../assets/css/demo.css"/>
+  <link rel="stylesheet" href="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css"/>
+  <link rel="stylesheet" href="../assets/vendor/css/pages/page-auth.css"/>
+  <script src="../assets/vendor/js/helpers.js"></script>
+  <script src="../assets/js/config.js"></script>
+  <style>
+    #toastBox {
+      position: absolute;
+      top: 30px;
+      right: 45.8%;
+      display: flex;
+      align-items: flex-end;
+      flex-direction: column;
+      overflow: hidden;
+      padding: 20px;
+      z-index: 3000;
+    }
 
-     .toast {
-         width: 400px;
-         height: 80px;
-         background: #fff;
-         margin: 15px 0;
-         box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
-         display: flex;
-         align-items: center;
-         position: relative;
-         transform: translateY(-100%);
-         opacity: 0.5;
-         animation: movetop 0.2s linear forwards;
-     }
+    .toast {
+      width: 400px;
+      height: 80px;
+      background: #fff;
+      margin: 15px 0;
+      box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+      display: flex;
+      align-items: center;
+      position: relative;
+      transform: translateY(-100%);
+      opacity: 0.5;
+      animation: movetop 0.2s linear forwards;
+    }
 
-     @keyframes movetop {
-         100% {
-             transform: translateY(0);
-             opacity: 100%;
-         }
-     }
+    @keyframes movetop {
+      100% {
+        transform: translateY(0);
+        opacity: 100%;
+      }
+    }
 
-     .toast i {
-         margin: 0 20px;
-         font-size: 35px;
-         color: green;
-     }
+    .toast i {
+      margin: 0 20px;
+      font-size: 35px;
+      color: green;
+    }
 
-     .toast.error i {
-         color: red;
-     }
+    .toast.error i {
+      color: red;
+    }
 
-     .toast.invalid i {
-         color: orange;
-     }
+    .toast.invalid i {
+      color: orange;
+    }
 
-     .toast::after {
-         content: '';
-         position: absolute;
-         left: 0;
-         bottom: 0;
-         width: 100%;
-         height: 5px;
-         background: green;
-         animation: anim 3s linear forwards;
-     }
+    .toast::after {
+      content: '';
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: 5px;
+      background: green;
+      animation: anim 3s linear forwards;
+    }
 
-     .toast.error::after {
-         background: red;
-     }
+    .toast.error::after {
+      background: red;
+    }
 
-     .toast.invalid::after {
-         background: orange;
-     }
+    .toast.invalid::after {
+      background: orange;
+    }
 
-     @keyframes anim {
-         100% {
-             width: 0;
-         }
-     }
- </style>
+    @keyframes anim {
+      100% {
+        width: 0;
+      }
+    }
+
+    @media (max-width: 502.9px) {
+      .cap-img {
+        width: 160px;
+      }
+    }
+
+    @media (max-width: 384px) {
+      #cap-container {
+        display: block;
+        width: 100%;
+      }
+      .cap-img {
+        width: 210px;
+        margin-top: 20px;
+      }
+      #captcha-code{
+        width: 210px;
+      }
+    }
+  </style>
 </head>
